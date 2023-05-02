8bd39cca6bd78e6321bc1f2055026f6921debdfe
Manuel Burdach <manuelburdach@outlook.de>
2023-05-02 12:09:09 +0200

    Upload

--- controller/userController.js
+++ controller/userController.js
@@ -30,12 +30,7 @@ export const verifyUser = async (req, res) => {
 // ---------------------------------------- LOGOUT USER
 // need: nothing
 export const logoutUser = async (req, res) => {
-  // res.clearCookie("token");
-  console.log(req.cookie);
-  res.cookie("token", "", {
-    expires: new Date(new Date().getTime() + 1000000000000000),
-    httpOnly: true,
-  });
+  res.clearCookie("token", { path: "/" });
   res.json({ firstname: "Guest", isLoggedIn: false });
 };
 
--- routes/users.js
+++ routes/users.js
@@ -22,7 +22,7 @@ user_router.get("/get", getUser);
 user_router.get("/verify", verifyToken, verifyUser);
 
 //logout
-user_router.post("/logout", logoutUser);
+user_router.get("/logout", logoutUser);
 
 // ---------------------------------------- POST
 // register
